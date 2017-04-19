009c86cf525e83d8db5d603a6eae09364f170e61
liujie <liujie@taovip.com>
2017-04-19 17:01:43 +0800

    del

--- mybatisDemo/target/classes/sql/order.sql
+++
@@ -1,7 +0,0 @@
-use demo;
-CREATE TABLE `demo`.`order` (
-  `id` BIGINT (11) NOT NULL AUTO_INCREMENT,
-  `detail` VARCHAR(255) NULL,
-  `buyer` VARCHAR(45) NULL,
-  `created_time` DATETIME NULL,
-  PRIMARY KEY (`id`));
